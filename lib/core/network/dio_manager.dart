8da33cfba3d3b5af0294f2ce4010383816b55265
Batuhan Battal <batubattal@gmail.com>
2025-03-04 16:00:19 +0300

    implement Dio Manager for Api Request

---
+++ lib/core/network/dio_manager.dart
@@ -0,0 +1,29 @@
+import 'package:cinemate_app/core/constant/api_constant.dart';
+import 'package:dio/dio.dart';
+
+class DioManager {
+  static final instance = DioManager._();
+  late final Dio dio;
+
+  DioManager._() {
+    dio = Dio(BaseOptions(
+      baseUrl: ApiConstant.baseUrl,
+      queryParameters: {
+        'api_key': ApiConstant.apiKey,
+      },
+      connectTimeout: const Duration(seconds: 5),
+      receiveTimeout: const Duration(seconds: 3),
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+    ));
+    dio.interceptors.add(LogInterceptor(
+      request: true,
+      requestHeader: true,
+      requestBody: true,
+      responseHeader: true,
+      responseBody: true,
+    ));
+  }
+}
