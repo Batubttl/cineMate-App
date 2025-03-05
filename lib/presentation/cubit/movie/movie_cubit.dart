6d67205892ed6c346f3642d148b66a65823ba4fe
Batuhan Battal <batubattal@gmail.com>
2025-03-05 14:39:36 +0300

    Favorites method added into project

--- lib/presentation/cubit/movie/movie_cubit.dart
+++ lib/presentation/cubit/movie/movie_cubit.dart
@@ -1,3 +1,4 @@
+import 'package:cinemate_app/core/enum/movie_list_type.dart';
 import 'package:cinemate_app/data/service/movie_service.dart';
 import 'package:cinemate_app/presentation/cubit/movie/movie_state.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -6,9 +7,9 @@ class MovieCubit extends Cubit<MovieState> {
   final MovieService _movieService;
   MovieCubit(this._movieService) : super(MovieState.initial());
 
+  // Trending Movies
   Future<void> getTrendingMovies() async {
     emit(state.copyWith(isLoading: true));
-
     try {
       final movie = await _movieService.getTrendingMovies();
       emit(state.copyWith(trendingMovies: movie.results, isLoading: false));
@@ -17,18 +18,18 @@ class MovieCubit extends Cubit<MovieState> {
     }
   }
 
+  // Popular Movies
   Future<void> getPopularMovies() async {
     emit(state.copyWith(isLoading: true));
-
     try {
       final movie = await _movieService.getPopularMovies();
-
       emit(state.copyWith(popularMovies: movie.results, isLoading: false));
     } catch (e) {
       emit(state.copyWith(errorMessage: e.toString(), isLoading: false));
     }
   }
 
+  // Top Rated Movies
   Future<void> getTopRatedMovies() async {
     emit(state.copyWith(isLoading: true));
     try {
@@ -37,5 +38,144 @@ class MovieCubit extends Cubit<MovieState> {
     } catch (e) {
       emit(state.copyWith(errorMessage: e.toString(), isLoading: false));
     }
+  }
+
+  // Movies by Genre
+  Future<void> getMoviesByGenre(int genreId) async {
+    emit(state.copyWith(isLoading: true));
+    try {
+      final movie = await _movieService.getMoviesByGenres(genreId);
+      emit(state.copyWith(moviesByGenre: movie.results, isLoading: false));
+    } catch (e) {
+      emit(state.copyWith(errorMessage: e.toString(), isLoading: false));
+    }
+  }
+
+  // Movie Detail
+  Future<void> getMovieDetail(int movieId) async {
+    emit(state.copyWith(isLoading: true));
+    try {
+      final movie = await _movieService.getMovieDetail(movieId);
+      emit(state.copyWith(selectedMovie: movie, isLoading: false));
+    } catch (e) {
+      emit(state.copyWith(errorMessage: e.toString(), isLoading: false));
+    }
+  }
+
+  // Search Movies
+  Future<void> searchMovies(String query) async {
+    if (query.isEmpty) {
+      emit(state.copyWith(searchResults: []));
+      return;
+    }
+
+    emit(state.copyWith(isSearching: true));
+    try {
+      final movie = await _movieService.searchMovies(query);
+      emit(state.copyWith(
+        searchResults: movie.results,
+        isSearching: false,
+      ));
+    } catch (e) {
+      emit(state.copyWith(
+        errorMessage: e.toString(),
+        isSearching: false,
+      ));
+    }
+  }
+
+  // Load More (Pagination)
+  Future<void> loadMore(MovieListType type, {int? genreId}) async {
+    if (state.isLoading) return;
+
+    try {
+      switch (type) {
+        case MovieListType.popular:
+          if (!state.hasMorePopularMovies) return;
+          final nextPage = (state.popularMovies?.length ?? 0) ~/ 20 + 1;
+          final movie = await _movieService.getPopularMovies(page: nextPage);
+          final updatedMovies = [
+            ...state.popularMovies ?? [],
+            ...movie.results
+          ];
+          emit(state.copyWith(
+            popularMovies: updatedMovies,
+            hasMorePopularMovies: movie.page < movie.totalPages,
+          ));
+
+        case MovieListType.trending:
+          if (!state.hasMoreTrendingMovies) return;
+          final nextPage = (state.trendingMovies?.length ?? 0) ~/ 20 + 1;
+          final movie = await _movieService.getTrendingMovies(page: nextPage);
+          final updatedMovies = [
+            ...state.trendingMovies ?? [],
+            ...movie.results
+          ];
+          emit(state.copyWith(
+            trendingMovies: updatedMovies,
+            hasMoreTrendingMovies: movie.page < movie.totalPages,
+          ));
+
+        case MovieListType.topRated:
+          if (!state.hasMoreTopRatedMovies) return;
+          final nextPage = (state.topRatedMovies?.length ?? 0) ~/ 20 + 1;
+          final movie = await _movieService.getTopRatedMovies(page: nextPage);
+          final updatedMovies = [
+            ...state.topRatedMovies ?? [],
+            ...movie.results
+          ];
+          emit(state.copyWith(
+            topRatedMovies: updatedMovies,
+            hasMoreTopRatedMovies: movie.page < movie.totalPages,
+          ));
+
+        case MovieListType.byGenre:
+          if (!state.hasMoreMoviesByGenre || genreId == null) return;
+          final nextPage = (state.moviesByGenre?.length ?? 0) ~/ 20 + 1;
+          final movie =
+              await _movieService.getMoviesByGenres(genreId, page: nextPage);
+          final updatedMovies = [
+            ...state.moviesByGenre ?? [],
+            ...movie.results
+          ];
+          emit(state.copyWith(
+            moviesByGenre: updatedMovies,
+            hasMoreMoviesByGenre: movie.page < movie.totalPages,
+          ));
+      }
+    } catch (e) {
+      emit(state.copyWith(errorMessage: e.toString()));
+    }
+  }
+
+  Future<void> refreshMovies(MovieListType type, {int? genreId}) async {
+    switch (type) {
+      case MovieListType.trending:
+        emit(state.copyWith(trendingMovies: []));
+        await getTrendingMovies();
+      case MovieListType.popular:
+        emit(state.copyWith(popularMovies: []));
+        await getPopularMovies();
+      case MovieListType.topRated:
+        emit(state.copyWith(topRatedMovies: []));
+        await getTopRatedMovies();
+      case MovieListType.byGenre:
+        if (genreId != null) {
+          emit(state.copyWith(moviesByGenre: []));
+          await getMoviesByGenre(genreId);
+        }
+    }
+  }
+
+  void clearError() {
+    emit(state.copyWith(errorMessage: null));
+  }
+
+  void clearSearchResults() {
+    emit(state.copyWith(searchResults: []));
+  }
+
+  void clearSelectedMovie() {
+    emit(state.copyWith(selectedMovie: null));
   }
 }
--- lib/presentation/cubit/movie/movie_state.dart
+++ lib/presentation/cubit/movie/movie_state.dart
@@ -1,3 +1,5 @@
+import 'package:cinemate_app/data/model/favorite_model.dart';
+import 'package:cinemate_app/data/model/movie_detail_model.dart';
 import 'package:cinemate_app/data/model/movie_model.dart';
 import 'package:equatable/equatable.dart';
 
@@ -6,40 +8,85 @@ class MovieState extends Equatable {
   final List<MovieResults>? trendingMovies;
   final List<MovieResults>? topRatedMovies;
   final List<MovieResults>? moviesByGenre;
+  final List<MovieResults>? searchResults;
+  final List<FavoriteItem>? favorites;
+
+  final MovieDetail? selectedMovie;
+  final bool isFavorite;
+
   final bool isLoading;
+  final bool isSearching;
+
+  final bool hasMorePopularMovies;
+  final bool hasMoreTrendingMovies;
+  final bool hasMoreTopRatedMovies;
+  final bool hasMoreMoviesByGenre;
+
   final String? errorMessage;
 
   const MovieState({
-    required this.popularMovies,
-    required this.trendingMovies,
-    required this.topRatedMovies,
-    required this.moviesByGenre,
+    this.favorites,
+    this.isFavorite = false,
+    this.popularMovies,
+    this.trendingMovies,
+    this.topRatedMovies,
+    this.moviesByGenre,
+    this.searchResults,
+    this.selectedMovie,
     required this.isLoading,
+    this.isSearching = false,
+    this.hasMorePopularMovies = true,
+    this.hasMoreTrendingMovies = true,
+    this.hasMoreTopRatedMovies = true,
+    this.hasMoreMoviesByGenre = true,
     this.errorMessage,
   });
 
   factory MovieState.initial() => const MovieState(
-        popularMovies: [],
-        trendingMovies: [],
-        topRatedMovies: [],
-        moviesByGenre: [],
-        isLoading: false,
-      );
+      popularMovies: [],
+      trendingMovies: [],
+      topRatedMovies: [],
+      moviesByGenre: [],
+      searchResults: [],
+      isLoading: false,
+      favorites: []);
+
   MovieState copyWith({
     List<MovieResults>? popularMovies,
     List<MovieResults>? trendingMovies,
     List<MovieResults>? topRatedMovies,
     List<MovieResults>? moviesByGenre,
+    List<MovieResults>? searchResults,
+    List<FavoriteItem>? favorites,
+    MovieDetail? selectedMovie,
     bool? isLoading,
+    bool? isSearching,
+    bool? isFavorite,
+    bool? hasMorePopularMovies,
+    bool? hasMoreTrendingMovies,
+    bool? hasMoreTopRatedMovies,
+    bool? hasMoreMoviesByGenre,
     String? errorMessage,
   }) =>
       MovieState(
-          popularMovies: popularMovies ?? this.popularMovies,
-          trendingMovies: trendingMovies ?? this.trendingMovies,
-          topRatedMovies: topRatedMovies ?? this.topRatedMovies,
-          moviesByGenre: moviesByGenre ?? this.moviesByGenre,
-          isLoading: isLoading ?? this.isLoading,
-          errorMessage: errorMessage ?? this.errorMessage);
+        popularMovies: popularMovies ?? this.popularMovies,
+        trendingMovies: trendingMovies ?? this.trendingMovies,
+        topRatedMovies: topRatedMovies ?? this.topRatedMovies,
+        moviesByGenre: moviesByGenre ?? this.moviesByGenre,
+        searchResults: searchResults ?? this.searchResults,
+        selectedMovie: selectedMovie ?? this.selectedMovie,
+        isLoading: isLoading ?? this.isLoading,
+        isSearching: isSearching ?? this.isSearching,
+        isFavorite: isFavorite ?? this.isFavorite,
+        favorites: favorites,
+        hasMorePopularMovies: hasMorePopularMovies ?? this.hasMorePopularMovies,
+        hasMoreTrendingMovies:
+            hasMoreTrendingMovies ?? this.hasMoreTrendingMovies,
+        hasMoreTopRatedMovies:
+            hasMoreTopRatedMovies ?? this.hasMoreTopRatedMovies,
+        hasMoreMoviesByGenre: hasMoreMoviesByGenre ?? this.hasMoreMoviesByGenre,
+        errorMessage: errorMessage ?? this.errorMessage,
+      );
 
   @override
   List<Object?> get props => [
@@ -47,7 +94,16 @@ class MovieState extends Equatable {
         trendingMovies,
         topRatedMovies,
         moviesByGenre,
+        searchResults,
+        selectedMovie,
+        favorites,
+        isFavorite,
         isLoading,
+        isSearching,
+        hasMorePopularMovies,
+        hasMoreTrendingMovies,
+        hasMoreTopRatedMovies,
+        hasMoreMoviesByGenre,
         errorMessage,
       ];
 }
