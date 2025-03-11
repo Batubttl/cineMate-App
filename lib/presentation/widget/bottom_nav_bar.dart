e619a6bc9338c88b928237edb14266cf56338a23
Batuhan Battal <batubattal@gmail.com>
2025-03-11 16:47:06 +0300

    implement router into bottom navigation bar

--- lib/presentation/widget/bottom_nav_bar.dart
+++ lib/presentation/widget/bottom_nav_bar.dart
@@ -1,23 +1,43 @@
 // bottom_nav_bar.dart
 import 'package:cinemate_app/core/constant/app_constant.dart';
+import 'package:cinemate_app/core/theme/app_colors.dart';
 import 'package:cinemate_app/presentation/cubit/navigation/navigation_cubit.dart';
 import 'package:cinemate_app/presentation/cubit/navigation/navigation_state.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
 
 class BottomNavBar extends StatelessWidget {
+  const BottomNavBar({super.key});
+
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<NavigationCubit, NavigationState>(
       builder: (context, state) {
         return BottomNavigationBar(
-          // Hangi sayfa aktif?
           currentIndex: state.currentIndex,
+          onTap: (index) {
+            context.read<NavigationCubit>().changeIndex(index);
 
-          // Tıklanınca ne olacak?
-          onTap: (index) => context.read<NavigationCubit>().changeIndex(index),
-
-          // Menü öğeleri
+            switch (index) {
+              case 0:
+                context.go('/');
+                break;
+              case 1:
+                context.go('/search');
+                break;
+              case 2:
+                context.go('/watchlist');
+                break;
+              case 3:
+                context.go('/favorites');
+                break;
+            }
+          },
+          type: BottomNavigationBarType.fixed,
+          backgroundColor: AppColors.primaryColor,
+          selectedItemColor: AppColors.textPrimary,
+          unselectedItemColor: AppColors.textSecondary,
           items: const [
             BottomNavigationBarItem(
               icon: Icon(Icons.home),
@@ -30,6 +50,10 @@ class BottomNavBar extends StatelessWidget {
             BottomNavigationBarItem(
               icon: Icon(Icons.bookmark),
               label: AppString.myListPage,
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.favorite),
+              label: AppString.favoritePage,
             ),
           ],
         );
