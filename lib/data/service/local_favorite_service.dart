a537d5762f105d83e7146764aead2aca82fa9f47
Batuhan Battal <batubattal@gmail.com>
2025-03-11 16:46:39 +0300

    move new methor from movie service to local favorite services

--- lib/data/service/local_favorite_service.dart
+++ lib/data/service/local_favorite_service.dart
@@ -1,7 +1,6 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'dart:convert';
-
 import 'package:cinemate_app/core/constant/app_constant.dart';
+import 'package:cinemate_app/core/enum/media_type.dart';
 import 'package:cinemate_app/data/model/favorite_model.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -9,9 +8,8 @@ class LocalFavoriteService {
   static const _favoritesKey = 'local_favorite';
   static const _lastUpdatedKey = 'local_favorite_last_updated';
   final SharedPreferences _prefs;
-  LocalFavoriteService({
-    required SharedPreferences prefs,
-  }) : _prefs = prefs;
+
+  LocalFavoriteService(this._prefs);
 
   Future<List<FavoriteItem>> getFavorites() async {
     try {
@@ -23,25 +21,31 @@ class LocalFavoriteService {
       final response = FavoriteResponse.fromJson(jsonMap);
       return response.results;
     } catch (e) {
-      throw Exception('Error getFavorites: $e');
+      throw Exception('${AppString.errorFavorites}: $e');
+    }
+  }
+
+  Future<List<FavoriteItem>> getFavoritesByType(MediaType type) async {
+    try {
+      final allFavorites = await getFavorites();
+      return allFavorites.where((item) => item.mediaType == type).toList();
+    } catch (e) {
+      throw Exception('${AppString.errorFavorites}: $e');
     }
   }
 
   Future<void> _saveFavorites(List<FavoriteItem> favorites) async {
     try {
       final response = FavoriteResponse(
-          page: 1,
-          results: favorites,
-          totalPages: 1,
-          totalResults: favorites.length);
+        page: 1,
+        results: favorites,
+        totalPages: 1,
+        totalResults: favorites.length,
+      );
 
-      final jsonString = json.encode({
-        'page': response.page,
-        'results': response.results.map((e) => e.toJson()).toList(),
-        'total_pages': response.totalPages,
-        'total_results': response.totalResults,
-      });
+      final jsonString = json.encode(response.toJson());
       await _prefs.setString(_favoritesKey, jsonString);
+      await _updateLastUpdated();
     } catch (e) {
       throw Exception('${AppString.errorSaveFavorited}: $e');
     }
@@ -50,10 +54,80 @@ class LocalFavoriteService {
   Future<void> addFavorite(FavoriteItem item) async {
     try {
       final currentFavorites = await getFavorites();
-      if (!currentFavorites.any((favorite) => favorite.id == item.id)) {
+      if (!currentFavorites.any(
+        (favorite) =>
+            favorite.id == item.id && favorite.mediaType == item.mediaType,
+      )) {
         currentFavorites.add(item);
         await _saveFavorites(currentFavorites);
       }
-    } catch (e) {}
+    } catch (e) {
+      throw Exception('${AppString.errorAddFavorite}: $e');
+    }
+  }
+
+  Future<void> removeFavorite(int id, MediaType type) async {
+    try {
+      final currentFavorites = await getFavorites();
+      currentFavorites.removeWhere(
+        (favorite) => favorite.id == id && favorite.mediaType == type,
+      );
+      await _saveFavorites(currentFavorites);
+    } catch (e) {
+      throw Exception('${AppString.errorRemoveFavorited}: $e');
+    }
+  }
+
+  Future<bool> isFavorite(int id, MediaType type) async {
+    try {
+      final favorites = await getFavorites();
+      return favorites.any((item) => item.id == id && item.mediaType == type);
+    } catch (e) {
+      throw Exception('${AppString.errorCheckIsFavorites}: $e');
+    }
+  }
+
+  Future<int> getFavoriteCount({MediaType? type}) async {
+    try {
+      if (type != null) {
+        final typeSpecificFavorites = await getFavoritesByType(type);
+        return typeSpecificFavorites.length;
+      }
+      final currentFavorites = await getFavorites();
+      return currentFavorites.length;
+    } catch (e) {
+      throw Exception('${AppString.errorGetFavoriteCount}: $e');
+    }
+  }
+
+  Future<void> clearFavorites() async {
+    try {
+      await _prefs.remove(_favoritesKey);
+      await _prefs.remove(_lastUpdatedKey);
+    } catch (e) {
+      throw Exception('${AppString.errorClearFavorites}: $e');
+    }
+  }
+
+  Future<void> clearFavoritesByType(MediaType type) async {
+    try {
+      final currentFavorites = await getFavorites();
+      final filteredFavorites =
+          currentFavorites.where((item) => item.mediaType != type).toList();
+      await _saveFavorites(filteredFavorites);
+    } catch (e) {
+      throw Exception('${AppString.errorClearFavorites}: $e');
+    }
+  }
+
+  DateTime? getLastUpdated() {
+    final timestamp = _prefs.getInt(_lastUpdatedKey);
+    return timestamp != null
+        ? DateTime.fromMillisecondsSinceEpoch(timestamp)
+        : null;
+  }
+
+  Future<void> _updateLastUpdated() async {
+    await _prefs.setInt(_lastUpdatedKey, DateTime.now().millisecondsSinceEpoch);
   }
 }
--- lib/data/service/movie_service.dart
+++ lib/data/service/movie_service.dart
@@ -1,4 +1,6 @@
+import 'package:cinemate_app/core/constant/api_constant.dart';
 import 'package:cinemate_app/core/constant/app_constant.dart';
+import 'package:cinemate_app/core/extension/api_path_extension.dart';
 import 'package:cinemate_app/data/model/favorite_model.dart';
 import 'package:cinemate_app/data/model/genre_model.dart';
 import 'package:cinemate_app/data/model/movie_detail_model.dart';
@@ -7,166 +9,122 @@ import 'package:dio/dio.dart';
 
 class MovieService {
   final Dio _dio;
+
   MovieService(this._dio);
 
+  // Popular Movies
   Future<Movie> getPopularMovies({int page = 1}) async {
     try {
-      final response = await _dio.get('/movie/popular', queryParameters: {
-        'language': 'tr-TR',
-        'page': page,
-      });
-      if (response.statusCode == 200) {
-        return Movie.fromJson(response.data);
-      } else {
-        throw Exception(
-            '${AppString.errorPopularMovies} :{response.statusCode}');
-      }
+      final response = await _dio.get(
+        ApiConstant.popularPath,
+        queryParameters: {
+          'language': 'tr-TR',
+          'page': page,
+        },
+      );
+      return Movie.fromJson(response.data);
     } catch (e) {
       throw Exception('${AppString.errorPopularMovies}: $e');
     }
   }
 
+  // Trending Movies
   Future<Movie> getTrendingMovies({int page = 1}) async {
     try {
-      final response = await _dio.get('/trending/movie/day', queryParameters: {
-        'language': 'tr-TR',
-        'page': page,
-      });
-      if (response.statusCode == 200) {
-        return Movie.fromJson(response.data);
-      } else {
-        throw Exception(
-            '${AppString.errorTrendingMovies} :{response.statusCode}');
-      }
+      final response = await _dio.get(
+        ApiConstant.trendingPath,
+        queryParameters: {
+          'language': 'tr-TR',
+          'page': page,
+        },
+      );
+      return Movie.fromJson(response.data);
     } catch (e) {
-      throw Exception('${AppString.errorTrendingMovies} : $e');
+      throw Exception('${AppString.errorTrendingMovies}: $e');
     }
   }
 
+  // Top Rated Movies
   Future<Movie> getTopRatedMovies({int page = 1}) async {
     try {
-      final response = await _dio.get('/movie/top_rated', queryParameters: {
-        'language': 'tr-TR',
-        'page': page,
-      });
-      if (response.statusCode == 200) {
-        return Movie.fromJson(response.data);
-      } else {
-        throw Exception(
-            '${AppString.errorTopRatedMovies} :{response.statusCode}');
-      }
+      final response = await _dio.get(
+        ApiConstant.topRatedPath,
+        queryParameters: {
+          'language': 'tr-TR',
+          'page': page,
+        },
+      );
+      return Movie.fromJson(response.data);
     } catch (e) {
-      throw Exception('${AppString.errorTopRatedMovies} $e');
+      throw Exception('${AppString.errorTopRatedMovies}: $e');
     }
   }
 
-  Future<List<GenreModel>> getGenres() async {
-    try {
-      final response = await _dio.get('/genre/movie/list', queryParameters: {
-        'language': 'tr-TR',
-      });
-      if (response.statusCode == 200) {
-        final genreResponse = GenreResponse.fromJson(response.data);
-        return genreResponse.genres;
-      } else {
-        throw Exception(
-            '${AppString.errorMoviesByGenres}: ${response.statusCode}');
-      }
-    } catch (e) {
-      throw Exception('${AppString.errorMoviesByGenres}: $e');
-    }
-  }
-
-  Future<Movie> getMoviesByGenre(int genreId, {int page = 1}) async {
-    try {
-      final response = await _dio.get('/discover/movie', queryParameters: {
-        'language': 'tr-TR',
-        'with_genres': genreId,
-        'page': page,
-        'sort_by': 'popularity.desc',
-      });
-
-      if (response.statusCode == 200) {
-        return Movie.fromJson(response.data);
-      } else {
-        throw Exception(
-            '${AppString.errorMoviesByGenres}: ${response.statusCode}');
-      }
-    } catch (e) {
-      throw Exception('${AppString.errorMoviesByGenres}: $e');
-    }
-  }
-
+  // Movie Detail
   Future<MovieDetail> getMovieDetail(int movieId) async {
     try {
-      final response = await _dio.get('/movie/$movieId', queryParameters: {
-        'language': 'tr-TR',
-        'append_to_response': 'videos,credits,similar,recommendations',
-      });
-      if (response.statusCode == 200) {
-        return MovieDetail.fromJson(response.data);
-      } else {
-        throw Exception(
-            '${AppString.errorMovieDetail}: ${response.statusCode}');
-      }
+      final response = await _dio.get(
+        ApiConstant.movieDetailPath.withMovieId(movieId),
+        queryParameters: {
+          'language': 'tr-TR',
+          'append_to_response': 'videos,credits,similar,recommendations',
+        },
+      );
+      return MovieDetail.fromJson(response.data);
     } catch (e) {
       throw Exception('${AppString.errorMovieDetail}: $e');
     }
   }
 
+  // Genres
+  Future<List<GenreModel>> getGenres() async {
+    try {
+      final response = await _dio.get(
+        ApiConstant.movieGenresPath,
+        queryParameters: {
+          'language': 'tr-TR',
+        },
+      );
+
+      final genreResponse = GenreResponse.fromJson(response.data);
+      return genreResponse.genres;
+    } catch (e) {
+      throw Exception('${AppString.errorGenres}: $e');
+    }
+  }
+
+  // Movies by Genre
+  Future<Movie> getMoviesByGenre(int genreId, {int page = 1}) async {
+    try {
+      final response = await _dio.get(
+        ApiConstant.discoverMoviePath.withGenreId(genreId),
+        queryParameters: {
+          'language': 'tr-TR',
+          'page': page,
+          'sort_by': 'popularity.desc',
+        },
+      );
+      return Movie.fromJson(response.data);
+    } catch (e) {
+      throw Exception('${AppString.errorMoviesByGenres}: $e');
+    }
+  }
+
+  // Search Movies
   Future<Movie> searchMovies(String query, {int page = 1}) async {
     try {
-      final response = await _dio.get('/search/movie', queryParameters: {
-        'query': query,
-        'language': 'tr-TR',
-        'page': page,
-        'include_adult': false,
-      });
-      if (response.statusCode == 200) {
-        return Movie.fromJson(response.data);
-      } else {
-        throw Exception('${AppString.errorSearch}: ${response.statusCode}');
-      }
+      final response = await _dio.get(
+        ApiConstant.searchMoviePath,
+        queryParameters: {
+          'query': query,
+          'language': 'tr-TR',
+          'page': page,
+          'include_adult': false,
+        },
+      );
+      return Movie.fromJson(response.data);
     } catch (e) {
       throw Exception('${AppString.errorSearch}: $e');
-    }
-  }
-
-  Future<List<FavoriteItem>> getFavorites() async {
-    try {
-      final movieResponse =
-          await _dio.get('/account/{account_id}/favorite/movies');
-      final movies = FavoriteResponse.fromJson(movieResponse.data);
-      final tvResponse = await _dio.get('/account/{account_id}/favorite/tv');
-      final tvShow = FavoriteResponse.fromJson(tvResponse.data);
-      return [...movies.results, ...tvShow.results];
-    } catch (e) {
-      throw Exception('${AppString.errorFavorites}: $e');
-    }
-  }
-
-  Future<void> toggleFavorite(
-      {required int mediaId,
-      required bool isFavorite,
-      required String accountId}) async {
-    try {
-      final response = await _dio.post('/account/$accountId/favorite', data: {
-        'media_type': 'movie',
-        'media_id': mediaId,
-        'favorite': isFavorite,
-      });
-      return response.data;
-    } catch (e) {
-      throw Exception('${AppString.errorToggleFavorite}: $e');
-    }
-  }
-
-  Future<bool> checkIsFavorites(int mediaId) async {
-    try {
-      final favorites = await getFavorites();
-      return favorites.any((element) => element.id == mediaId);
-    } catch (e) {
-      throw Exception('${AppString.errorCheckIsFavorites}: $e');
     }
   }
 }
