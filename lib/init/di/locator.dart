8e8a820d3d3dcf6f7ea8d104bd6a4c1a19f7ee5e
Batubttl <batubattal@gmail.com>
2025-04-09 00:39:58 +0300

    Main and locator reorganized

--- lib/init/di/locator.dart
+++ lib/init/di/locator.dart
@@ -1,25 +1,52 @@
 import 'package:cinemate_app/core/network/dio/dio_manager.dart';
+import 'package:cinemate_app/data/service/local_favorite_service.dart';
+import 'package:cinemate_app/data/service/movie_detail_service.dart';
 import 'package:cinemate_app/data/service/movie_service.dart';
 import 'package:cinemate_app/data/service/person_service.dart';
 import 'package:cinemate_app/data/service/search_service.dart';
 import 'package:cinemate_app/data/service/tv_service.dart';
-import 'package:cinemate_app/presentation/cubit/movie/movie_cubit.dart'; // MovieCubit import edilmeli
+import 'package:cinemate_app/init/navigation/navigation_cubit.dart';
+import 'package:cinemate_app/presentation/detail/cubit/detail_cubit.dart';
+import 'package:cinemate_app/presentation/favorite/cubit/favorite_cubit.dart';
+import 'package:cinemate_app/presentation/home/cubit/movie_cubit.dart';
+import 'package:cinemate_app/presentation/search/cubit/search_cubit.dart';
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 final locator = GetIt.instance;
 
-void setupLocator() {
+Future<void> setupLocator() async {
   locator.registerLazySingleton<Dio>(() => DioManager.instance.dio);
 
   locator
       .registerLazySingleton<MovieService>(() => MovieService(locator<Dio>()));
   locator.registerLazySingleton<TVService>(() => TVService(locator<Dio>()));
   locator.registerLazySingleton<SearchService>(
-      () => SearchService(locator<Dio>()));
+    () => SearchService(locator<Dio>()),
+  );
   locator.registerLazySingleton<PersonService>(
-      () => PersonService(locator<Dio>()));
+    () => PersonService(locator<Dio>()),
+  );
+
+  final sharedPreferences = await SharedPreferences.getInstance();
+  locator.registerLazySingleton<SharedPreferences>(() => sharedPreferences);
+
+  locator.registerLazySingleton<LocalFavoriteService>(
+    () => LocalFavoriteService(locator<SharedPreferences>()),
+  );
+
+  locator.registerLazySingleton<FavoriteCubit>(
+    () => FavoriteCubit(locator<LocalFavoriteService>()),
+  );
+  locator.registerLazySingleton(() => MovieDetailService(locator()));
+
+  locator.registerFactory(() => DetailCubit(locator()));
 
   locator.registerLazySingleton<MovieCubit>(
-      () => MovieCubit(locator<MovieService>()));
+    () => MovieCubit(movieService: locator<MovieService>()),
+  );
+  locator.registerLazySingleton<NavigationCubit>(() => NavigationCubit());
+
+  locator.registerFactory(() => SearchCubit(locator<MovieService>()));
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,36 @@
 import 'package:cinemate_app/core/constant/app_constant.dart';
+import 'package:cinemate_app/core/router/app_router.dart';
+import 'package:cinemate_app/core/theme/app_theme.dart';
 import 'package:cinemate_app/init/di/locator.dart';
-import 'package:cinemate_app/presentation/pages/home_page.dart';
+import 'package:cinemate_app/init/navigation/navigation_cubit.dart';
+import 'package:cinemate_app/presentation/favorite/cubit/favorite_cubit.dart';
+import 'package:cinemate_app/presentation/home/cubit/movie_cubit.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:cinemate_app/presentation/cubit/movie/movie_cubit.dart';
-import 'core/theme/app_theme.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await dotenv.load(fileName: ".env");
-  setupLocator();
-  runApp(const MyApp());
-}
+  await dotenv.load(fileName: '.env');
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  await setupLocator();
 
-  @override
-  Widget build(BuildContext context) {
-    return MultiBlocProvider(
+  final navigationCubit = NavigationCubit();
+  final appRouter = AppRouter(navigationCubit);
+
+  runApp(
+    MultiBlocProvider(
       providers: [
-        BlocProvider<MovieCubit>(
-          create: (context) => locator<MovieCubit>(),
-        ),
+        BlocProvider(create: (_) => navigationCubit),
+        BlocProvider(create: (_) => locator<MovieCubit>()..loadHomePageData()),
+        BlocProvider(create: (_) => locator<FavoriteCubit>()),
       ],
-      child: MaterialApp(
+      child: MaterialApp.router(
         title: AppString.appName,
         debugShowCheckedModeBanner: false,
         theme: AppTheme.darkTheme,
-        home: const HomePage(),
+        routerConfig: appRouter.router,
       ),
-    );
-  }
+    ),
+  );
 }
